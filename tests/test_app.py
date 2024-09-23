9fea0d8d25299cae2ce7781f606710b49e52d1d3
NickolasSilva01 <nickolas.silva@ottech.com.br>
2024-09-23 11:18:00 -0300

    aula 2 e exercicio

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,23 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
     assert response.json() == {'message': 'Olá mundo!'}  # Assert (Afirmação)
+
+
+def test_read_root_html_deve_retornar_ok_e_uma_pagina_html():
+    client = TestClient(app)  # Arrange (Organização do teste)
+
+    response = client.get('/html')  # Act (Ação do teste)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title> Teste de retorno HTML </title>
+        </head>
+        <body>
+            <h1> Meu primeiro teste sozinho! </h1>
+        </body>
+    """
+    )  # Assert (Afirmação)
--- to_do_list/app.py
+++ to_do_list/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+        <head>
+            <title> Teste de retorno HTML </title>
+        </head>
+        <body>
+            <h1> Meu primeiro teste sozinho! </h1>
+        </body>
+    """
